71a375808452bb366648831389fc015685b4b547
Derek Enos <derek.enos@gmail.com>
2020-12-25 16:39:52 -0500

    Moved filesystem endpoints behind filesystem_http_endpoints.attach(), made filesystem ops relative to some public_root to prevent root filesystem access, and beefed up directory listing interface.

--- serve.py
+++ serve.py
@@ -1,10 +1,11 @@
 
 import asyncio
 
-import default_http_endpoints
+import filesystem_http_endpoints
 from server import serve
 
 if __name__ == '__main__':
+    filesystem_http_endpoints.attach()
     event_loop = asyncio.get_event_loop()
     event_loop.create_task(serve())
     event_loop.run_forever()
